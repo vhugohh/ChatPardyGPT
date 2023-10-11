a3a2532872d22c60731d777a0d0af4104e6ca299
vhugohh <vhugohh@hotmail.com>
2023-10-11 10:45:28 -0600

    Create checkboxcolumn.py

---
+++ Streamlit/checkboxcolumn.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import streamlit as st
+
+data_df = pd.DataFrame(
+    {
+        "widgets": ["st.selectbox", "st.number_input", "st.text_area", "st.button"],
+        "favorite": [True, False, False, True],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "favorite": st.column_config.CheckboxColumn(
+            "Your favorite?",
+            help="Select your **favorite** widgets",
+            default=False,
+        )
+    },
+    disabled=["widgets"],
+    hide_index=True,
+)
