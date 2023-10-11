152963af1102328d625c468530e5e591d33f9099
vhugohh <vhugohh@hotmail.com>
2023-10-11 10:56:49 -0600

    Update map.py

--- Streamlit/map.py
+++ Streamlit/map.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame({
-    "col1": np.random.randn(1000) / 50 + 18.64,
-    "col2": np.random.randn(1000) / 50 + -91.82,
+    "col1": np.random.randn(1000) / 18.64,
+    "col2": np.random.randn(1000) / -91.82,
     "col3": np.random.randn(1000) * 100,
     "col4": np.random.rand(1000, 4).tolist(),
 })
